a37a0473d409f69b1b11f8959bbfa18b89a01483
Ming <mingma0626@gmail.com>
2023-11-06 15:25:18 -0800

    test

--- src/test/java/pantrypal/RecipeTest.java
+++ src/test/java/pantrypal/RecipeTest.java
@@ -1,43 +1,39 @@
-// package pantrypal;
+package pantrypal;
 
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// import java.io.IOException;
-// import java.net.URISyntaxException;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
-// import static org.junit.jupiter.api.Assertions.assertFalse;
-// import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// public class RecipeTest {
-//     // @Test
-//     // public void testRecipeCreation() {
-//     //     Recipe recipe = new Recipe();
-//     //     assertNotNull(recipe);
-//     // }
+public class RecipeTest {
+    private Recipe recipe;
+    @BeforeEach
+    void setUp() {recipe = new Recipe();
+    }
+    
+    @Test
+    void testSetRecipeIndex() {
+        recipe.setRecipeIndex(1);
+        assertEquals("1", recipe.index.getText());
+    }
 
-//     @Test
-//     public void testSetRecipeIndex() {
-//         Recipe recipe = new Recipe();
-//         recipe.setRecipeIndex(1);
-//         assertEquals("1", recipe.index.getText());
-//     }
+    @Test
+    void testSetRecipeName() 
+        recipe.setRecipeName("Pasta");
+        assertEquals("Pasta", recipe.recipeName.getText());
+    }
 
-//     @Test
-//     public void testSetRecipeName() {
-//         Recipe recipe = new Recipe();
-//         recipe.setRecipeName("Pasta");
-//         assertEquals("Pasta", recipe.recipeName.getText());
-//     }
+    @Test
+    void testSetRecipeDetails() {
+        recipe.setRecipeDetails("Ingredients: Flour, Water");
+        assertEquals("Ingredients: Flour, Water", recipe.recipeDetails.getText());
+    }
 
-//     @Test
-//     public void testSetRecipeDetails() {
-//         Recipe recipe = new Recipe();
-//         recipe.setRecipeDetails("Ingredients: Flour, Water");
-//         assertEquals("Ingredients: Flour, Water", recipe.recipeDetails.getText());
-//     }
-
-// }
+}
