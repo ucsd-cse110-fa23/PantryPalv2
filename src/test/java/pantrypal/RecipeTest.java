4efe9084ac09a4d1a03d24536f38cfb3583ad555
Ming <mingma0626@gmail.com>
2023-11-06 15:54:22 -0800

    test

--- src/test/java/pantrypal/RecipeTest.java
+++ src/test/java/pantrypal/RecipeTest.java
@@ -1,43 +1,43 @@
-package pantrypal;
+// package pantrypal;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+// import java.io.IOException;
+// import java.net.URISyntaxException;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+// import static org.junit.jupiter.api.Assertions.assertFalse;
+// import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import pantrypal.Main;
+// import pantrypal.Main;
 
-public class RecipeTest {
-    private Recipe recipe;
+// public class RecipeTest {
+//     private Recipe recipe;
 
-    @BeforeEach
-    void setUp() {
-        recipe = new Recipe();
-    }
+//     @BeforeEach
+//     void setUp() {
+//         recipe = new Recipe();
+//     }
     
-    @Test
-    void testSetRecipeIndex() {
-        recipe.setRecipeIndex(1);
-        assertEquals("1", recipe.index.getText());
-    }
+//     @Test
+//     void testSetRecipeIndex() {
+//         recipe.setRecipeIndex(1);
+//         assertEquals("1", recipe.index.getText());
+//     }
 
-    @Test
-    void testSetRecipeName() {
-        recipe.setRecipeName("Pasta");
-        assertEquals("Pasta", recipe.recipeName.getText());
-    }
+//     @Test
+//     void testSetRecipeName() {
+//         recipe.setRecipeName("Pasta");
+//         assertEquals("Pasta", recipe.recipeName.getText());
+//     }
 
-    @Test
-    void testSetRecipeDetails() {
-        recipe.setRecipeDetails("Ingredients: Flour, Water");
-        assertEquals("Ingredients: Flour, Water", recipe.recipeDetails.getText());
-    }
+//     @Test
+//     void testSetRecipeDetails() {
+//         recipe.setRecipeDetails("Ingredients: Flour, Water");
+//         assertEquals("Ingredients: Flour, Water", recipe.recipeDetails.getText());
+//     }
 
-}
+// }
--- src/test/java/pantrypal/WhisperTest.java
+++ src/test/java/pantrypal/WhisperTest.java
@@ -13,6 +13,26 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class WhisperTest {
-    
+    private Whisper whisper;
+
+    @BeforeEach
+    public void setUp() {
+        whisper = new Whisper();
+    }
+
+    @Test
+    public void testGetTranscript() {
+        try {
+            String filePath = "./my_audio.m4a"; 
+            String transcript = whisper.getTranscript(filePath);
+
+            
+            assertNotNull(transcript);
+            assertTrue(transcript.length() > 0);
+
+        } catch (IOException | URISyntaxException e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
 
 }
