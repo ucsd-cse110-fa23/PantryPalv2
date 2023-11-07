7bd3cd75461d122274c02b620b6c60ae3bb0a37e
Alon Lahav <33399838+alonlahav123@users.noreply.github.com>
2023-11-06 19:34:44 -0800

    cleaned unit tests

--- src/test/java/pantrypal/ChatGPTTest.java
+++ src/test/java/pantrypal/ChatGPTTest.java
@@ -10,8 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ChatGPTTest {
 
-
-
     @Test
     void testExtractIngredients() {
         String prompt = "{\"ingredients\": [\"Chicken\",\"Rice\"]}";
@@ -52,73 +50,4 @@ public class ChatGPTTest {
         assertEquals(test[1], output[1]);       
     }
 
-
-
-
-    // private MyQueue queue;
-    
-    // @BeforeEach
-    // void setUp() {
-    //     queue = new MyQueue(5);
-    // }
-    
-    // @Test
-    // void testEnqueue() {
-    //     queue.enqueue(1);
-    //     queue.enqueue(2);
-    //     queue.enqueue(3);
-    //     assertEquals(3, queue.size());
-    // }
-    
-    // @Test
-    // void testDequeue() {
-    //     queue.enqueue(1);
-    //     queue.enqueue(2);
-    //     queue.enqueue(3);
-    //     assertEquals(1, queue.dequeue());
-    //     assertEquals(2, queue.dequeue());
-    //     assertEquals(1, queue.size());
-    // }
-    
-    // @Test
-    // void testPeek() {
-    //     queue.enqueue(1);
-    //     queue.enqueue(2);
-    //     queue.enqueue(3);
-    //     assertEquals(1, queue.peek());
-    //     assertEquals(3, queue.size());
-    // }
-    
-    // @Test
-    // void testIsEmpty() {
-    //     assertTrue(queue.isEmpty());
-    //     queue.enqueue(1);
-    //     assertFalse(queue.isEmpty());
-    // }
-    
-    // @Test
-    // void testIsFull() {
-    //     assertFalse(queue.isFull());
-    //     queue.enqueue(1);
-    //     queue.enqueue(2);
-    //     queue.enqueue(3);
-    //     queue.enqueue(4);
-    //     queue.enqueue(5);
-    //     assertTrue(queue.isFull());
-    // }
-    
-    // @Test
-    // void testEnqueueWhenFull() {
-    //     queue.enqueue(1);
-    //     queue.enqueue(2);
-    //     queue.enqueue(3);
-    //     queue.enqueue(4);
-    //     queue.enqueue(5);
-    //     assertThrows(IllegalStateException.class, () -> queue.enqueue(6));
-    // }
-    
-    // @Test
-    // void testDequeueWhenEmpty() {
-    //     assertThrows(IllegalStateException.class, () -> queue.dequeue());
-    // }
 }
