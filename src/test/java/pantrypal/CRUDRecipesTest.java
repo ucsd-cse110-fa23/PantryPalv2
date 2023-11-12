f9a5c3a557a89c0b68ee02335118d9c860e165e6
Alon Lahav <33399838+alonlahav123@users.noreply.github.com>
2023-11-12 15:59:00 -0800

    added more testing

--- src/test/java/pantrypal/CRUDRecipesTest.java
+++ src/test/java/pantrypal/CRUDRecipesTest.java
@@ -3,10 +3,12 @@ package pantrypal;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -213,6 +215,123 @@ public class CRUDRecipesTest {
             updatedRecipe.instructions = instructionsUpdated;
             CRUDRecipes.updateRecipe(updatedRecipe);
             assertEquals(CRUDRecipes.getRecipe(title).instructions, instructionsUpdated);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testGetRecipe() {
+        String title = "Chicken and Rice";
+        String[] ingredients = {"Chicken", "Rice"};
+        String instructions = "Cook it up";
+        RecipeData recipe1 = new RecipeData(title, ingredients, instructions);
+
+        try {
+            CRUDRecipes.createRecipe(recipe1);
+
+            //Check that title, ingredients, and instructions are the same
+            assertEquals(CRUDRecipes.getRecipe(title).title, recipe1.title);
+            assertArrayEquals(CRUDRecipes.getRecipe(title).ingredients, recipe1.ingredients);
+            assertEquals(CRUDRecipes.getRecipe(title).instructions, recipe1.instructions);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testGetRecipeDifferentRecipes() {
+        String title1 = "Chicken and Rice";
+        String[] ingredients1 = {"Chicken", "Rice"};
+        String instructions1 = "Cook it up";
+        RecipeData recipe1 = new RecipeData(title1, ingredients1, instructions1);
+
+        String title2 = "Beef and Rice";
+        String[] ingredients2 = {"Beef", "Rice"};
+        String instructions2 = "Dont cook it up";
+        RecipeData recipe2 = new RecipeData(title2, ingredients2, instructions2);
+
+        try {
+            CRUDRecipes.createRecipe(recipe1);
+            //Check that title, ingredients, and instructions are the same
+            assertNotEquals(CRUDRecipes.getRecipe(title1).title, recipe2.title);
+            assertNotEquals(CRUDRecipes.getRecipe(title1).instructions, recipe2.instructions);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testDeleteRecipe() {
+        String title1 = "Chicken and Rice";
+        String[] ingredients1 = {"Chicken", "Rice"};
+        String instructions1 = "Cook it up";
+        RecipeData recipe1 = new RecipeData(title1, ingredients1, instructions1);
+
+        try {
+            CRUDRecipes.createRecipe(recipe1);
+            
+            //Ensure recipe has been added
+            assertTrue(CRUDRecipes.recipeExists(title1));
+
+            CRUDRecipes.deleteRecipe(recipe1.title);
+
+            assertFalse(CRUDRecipes.recipeExists(title1));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testDeleteRecipe2() {
+        String title1 = "";
+        String[] ingredients1 = {};
+        String instructions1 = "";
+        RecipeData recipe1 = new RecipeData(title1, ingredients1, instructions1);
+
+        try {
+            CRUDRecipes.createRecipe(recipe1);
+            
+            //Ensure recipe has been added
+            assertTrue(CRUDRecipes.recipeExists(title1));
+
+            CRUDRecipes.deleteRecipe(recipe1.title);
+
+            assertFalse(CRUDRecipes.recipeExists(title1));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testReadRecipes() {
+        String title1 = "Chicken and Rice";
+        String[] ingredients1 = {"Chicken", "Rice"};
+        String instructions1 = "Cook it up";
+        RecipeData recipe1 = new RecipeData(title1, ingredients1, instructions1);
+
+        String title2 = "Beef and Rice";
+        String[] ingredients2 = {"Beef", "Rice"};
+        String instructions2 = "Dont cook it up";
+        RecipeData recipe2 = new RecipeData(title2, ingredients2, instructions2);
+
+        try {
+            CRUDRecipes.createRecipe(recipe1);
+            CRUDRecipes.createRecipe(recipe2);
+
+            ArrayList<RecipeData> expectedRecipes = new ArrayList<>();
+            expectedRecipes.add(recipe1);
+            expectedRecipes.add(recipe2);
+
+            ArrayList<RecipeData> resultRecipes = CRUDRecipes.readRecipes();
+
+            assertEquals(expectedRecipes.get(0).title, recipe1.title);
+            assertArrayEquals(expectedRecipes.get(0).ingredients, recipe1.ingredients);
+            assertEquals(expectedRecipes.get(0).instructions, recipe1.instructions);
+
+            assertEquals(expectedRecipes.get(1).title, recipe2.title);
+            assertArrayEquals(expectedRecipes.get(1).ingredients, recipe2.ingredients);
+            assertEquals(expectedRecipes.get(1).instructions, recipe2.instructions);
         } catch (IOException e) {
             e.printStackTrace();
         }
