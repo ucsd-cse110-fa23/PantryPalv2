059ada982bbfcd978dcc35a0af1cfb94d76599f1
Ming <mingma0626@gmail.com>
2023-11-12 18:14:20 -0800

    renamed

--- src/test/java/pantrypal/ViewRecipeListBDDTest.java
+++ src/test/java/pantrypal/ViewRecipeListBDDTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ViewRecipeListBBDTest {
+public class ViewRecipeListBDDTest {
     RecipeData savedRecipe;
 
     // Scenario 1: Saved at least one recipe
