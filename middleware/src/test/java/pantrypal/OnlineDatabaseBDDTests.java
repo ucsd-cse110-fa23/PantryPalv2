43ce6e03a8197dbdccac906654474a0d32420029
akkuduvalli <117036188+akkuduvalli@users.noreply.github.com>
2023-11-27 21:57:16 -0800

    added BDD test for deletion

--- middleware/src/test/java/pantrypal/OnlineDatabaseBDDTests.java
+++ middleware/src/test/java/pantrypal/OnlineDatabaseBDDTests.java
@@ -1,6 +1,7 @@
 package pantrypal;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import java.io.IOException;
 
@@ -45,4 +46,46 @@ public class OnlineDatabaseBDDTests {
         }
 
     }
+
+    @Test
+    void testBDDDeleteRecipe() {
+        // Given that the user has generated a recipe with the title “Miso Soup”, 
+        // the ingredients “Miso Paste, Tofu”, and instructions “Put all ingredients 
+        // into a pot of water and boil”, and an image of a bowl of soup with it
+
+        // When the user deletes that recipe and logs out of their account and 
+        // logs onto their account from another computer and clicks on the “View Recipe List” button, 
+
+        // Then the user should see an empty list of recipes
+
+        // creating the recipe with Miso Soup as part of the recipelist
+        RecipeData recipe = new RecipeData("Miso Soup", new String[] { "Miso Paste", "Tofu" },
+                "Put all ingredients into a pot of water and boil", "Lunch", 0.0);
+        RecipeService recipeService = new RecipeService("Ben", true);
+        try {
+            recipeService.createRecipe(recipe);
+            assertTrue(recipeService.recipeExists(recipe.title));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // When the user deletes that recipe and logs out of their account and 
+        // logs onto their account from another computer and clicks on the “View Recipe List” button, 
+
+
+        RecipeService recipeService2 = new RecipeService("Ben", true);
+        // Then the user should see a recipe with the title “Miso Soup”, the ingredients
+        // “Miso Paste, Tofu”, and instructions “Put all ingredients into a pot of water
+        // and boil”, and an image of a bowl of soup with it.
+        try {
+            assertTrue(recipeService2.recipeExists(recipe.title));
+            recipeService.deleteRecipe("Miso Soup"); 
+            assertFalse(recipeService2.recipeExists(recipe.title));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
