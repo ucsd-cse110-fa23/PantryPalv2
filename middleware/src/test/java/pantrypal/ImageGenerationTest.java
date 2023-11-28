a6ead716748a78b19571c89fae41f9c78324d241
Ming <mingma0626@gmail.com>
2023-11-28 01:46:36 -0800

    Update ImageGenerationTest.java

--- middleware/src/test/java/pantrypal/ImageGenerationTest.java
+++ middleware/src/test/java/pantrypal/ImageGenerationTest.java
@@ -1,6 +1,8 @@
 package pantrypal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,7 +20,7 @@ public class ImageGenerationTest {
     @BeforeEach
     public void setUp() {
         // Initialize the DallE object before each test execution
-        this.dalle = new DallE();
+        this.dalle = mock(DallE.class);
         // Define the basic structure of the URL, excluding dynamically generated parts
         this.urlPattern = Pattern.compile("https://oaidalleapiprodscus.blob.core.windows.net/private/org-.*/user-.*/img-.*\\.png.*");
     
@@ -26,14 +28,17 @@ public class ImageGenerationTest {
 
     @Test
     public void generateImageUrlNotNull() throws IOException, InterruptedException, URISyntaxException {
+        when(dalle.generateImageURL(anyString())).thenReturn("https://oaidalleapiprodscus.blob.core.windows.net/private/org-mocked/user-mocked/img-mocked.png?st=mocked&se=mocked&sp=mocked&sv=mocked&sr=mocked&rscd=mocked&rsct=image/png&skoid=mocked&sktid=mocked&skt=mocked&ske=mocked&sks=mocked&skv=mocked&sig=mocked");
         String recipeTitle = "Chicken fried rice";
         String imageUrl = dalle.generateImageURL(recipeTitle);
-        assertNotNull(imageUrl, "Generated image URL should not be null");
-    }
+        assertNotNull(imageUrl, "Generated image URL should not be null");}
     
     @Test
     public void generateImageUrlValidFormat() throws IOException, InterruptedException, URISyntaxException {
-        String recipeTitle = "Chicken fried rice";
+        // Set up behavior for the mocked DallE instance
+        when(dalle.generateImageURL(anyString())).thenReturn("https://oaidalleapiprodscus.blob.core.windows.net/private/org-mocked/user-mocked/img-mocked.png?st=mocked&se=mocked&sp=mocked&sv=mocked&sr=mocked&rscd=mocked&rsct=image/png&skoid=mocked&sktid=mocked&skt=mocked&ske=mocked&sks=mocked&skv=mocked&sig=mocked");
+
+        String recipeTitle = "Miso Soup";
         String imageUrl = dalle.generateImageURL(recipeTitle);
         assertTrue(urlPattern.matcher(imageUrl).matches(), "Invalid image URL format");
     }
