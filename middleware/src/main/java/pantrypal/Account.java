10ef3234371cc3dd8984f309425c782bd46530ae
Alon Lahav <33399838+alonlahav123@users.noreply.github.com>
2023-11-28 17:57:01 -0800

    added more files

---
+++ middleware/src/main/java/pantrypal/Account.java
@@ -0,0 +1,20 @@
+package pantrypal;
+
+class Account {
+
+    private String username;
+    private String password;
+
+    Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
