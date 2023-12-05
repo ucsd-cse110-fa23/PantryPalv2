39b772a47f0b09823d3fda5e5cf7b4a6667bcdfe
akkuduvalli <117036188+akkuduvalli@users.noreply.github.com>
2023-12-04 19:41:12 -0800

    fixed main

--- frontend/src/main/java/pantrypal/Main.java
+++ frontend/src/main/java/pantrypal/Main.java
@@ -766,23 +766,6 @@ class AppFrame extends BorderPane {
 public class Main extends Application {
 
     @Override
-    public void init() throws Exception {
-        System.out.println("Application is initializing...");
-
-        MiddlewareModel mm = new MiddlewareModel();
-        List<RecipeData> recipes = mm.getRecipes();
-
-        if (recipes == null) {
-            recipes = new ArrayList<RecipeData>();
-        }
-
-        for (RecipeData recipe : recipes) {
-            CRUDRecipes.createRecipe(recipe);
-        }
-
-    }
-
-    @Override
     public void stop() throws Exception {
         System.out.println("Application is closing...");
 
