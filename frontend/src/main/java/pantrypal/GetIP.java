bfb158e63c4134548dd7be030138a198fd69a3fd
2s2e <stevendiwenshi1113@gmail.com>
2023-12-05 20:26:20 -0800

    Added GetIP working

--- frontend/src/main/java/pantrypal/GetIP.java
+++ frontend/src/main/java/pantrypal/GetIP.java
@@ -6,7 +6,9 @@ import java.io.FileReader;
 public class GetIP {
     public static String getIP() {
         try (BufferedReader reader = new BufferedReader(new FileReader("src/resources/ip.txt"))) {
-            return reader.readLine(); // Read and return the first line
+            String toRet = reader.readLine().strip().trim();
+            System.out.println(toRet);
+            return toRet; // Read and return the first line
         } catch (Exception e) {
             System.out.println("Error reading ip from ip.txt");
             System.out.println(e);
